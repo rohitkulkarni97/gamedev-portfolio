0d18efbe8519b4193d09581e39143b1d9d256e5f
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-03-13 21:07:41 -0400

    Contact details added

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    
+
     <h1>Let's get in touch</h1>
 
-    <div style="margin-bottom:40px;">I'm looking for a job !<br/>Feel free to contact me about any opportunity, or just to chat about cooking, kittens or anything else.</div>
+    <div style="margin-bottom:40px;">Feel free to contact me about any opportunity, or just to chat about gamedev,
+      coding or anything else :)
+    </div>
 
     <ul>
       <li>
@@ -12,28 +14,28 @@
       </li>
       <li>
         <i class="fa fa-envelope-o fa-lg fa-fw"></i>
-        <a href="mailto:my@email.com" target="_blank">my@email.com</a>
+        <a href="mailto:rohit.kulkarni.271197@gmail.com" target="_blank">rohit.kulkarni.271197@gmail.com</a>
+        <i>,</i>
+        <a href="mailto:rk3507@g.rit.edu" target="_blank">rk3507@g.rit.edu</a>
       </li>
       <li>
         <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
-        <a href="https://www.linkedin.com/me" target="_blank">linkedin.com/me</a>
+        <a href="https://www.linkedin.com/in/rohit-kulkarni-b2ba50174/" target="_blank">Rohit Kulkarni</a>
       </li>
       <li>
         <i class="fa fa-github fa-lg fa-fw"></i>
-        <a href="https://github.com/me" target="_blank">github.com/me</a>
+        <a href="https://github.com/RohitK-RIT" target="_blank">RohitK-RIT</a>
+        <i>,</i>
+        <a href="https://github.com/rohitkulkarni97" target="_blank">rohitkulkarni97</a>
       </li>
       <li>
         <i class="fa fa-gamepad fa-lg fa-fw"></i>
-        <a href="https://myself.itch.io" target="_blank">myself.itch.io</a>
+        <a href="https://rohitkul97.itch.io/" target="_blank">rohitkul97</a>
       </li>
-      <li>
-        <i class="fa fa-tumblr fa-lg fa-fw"></i>
-        <a href="https://myblog.tumblr.com" target="_blank">myblog.tumblr.com</a>
-      </li>
-      <li>
-        <i class="fa fa-steam fa-lg fa-fw"></i>
-        <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
-      </li>
+      <!--      <li>-->
+      <!--        <i class="fa fa-twitter fa-lg fa-fw"></i>-->
+      <!--        <a href="https://twitter.com/thadaniheena_" target="_blank">X/heenathadani</a>-->
+      <!--      </li>-->
     </ul>
 
   </div>
@@ -46,11 +48,11 @@ ul {
 }
 
 li {
-    line-height: 1.8em;
-    font-size: 1.2em;
-  }
+  line-height: 1.8em;
+  font-size: 1.2em;
+}
 
-@media only screen and (min-width: 620px){
+@media only screen and (min-width: 620px) {
   ul {
     padding-inline-start: 40px;
   }
